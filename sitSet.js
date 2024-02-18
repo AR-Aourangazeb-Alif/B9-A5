47f682f3d415c60f36e3b36dcb325efd0c7df278
AR Aourangazeb Alif <ar.aourangazeb.alif@gmail.com>
2024-02-19 00:02:51 +0600

    sit

---
+++ sitSet.js
@@ -0,0 +1,39 @@
+let rowID = 0;
+
+
+for(let i = 0; i < 40; i++){
+
+    let sitNumber = i%4;
+
+    const rowString = "ABCDEFGHIJ";
+    const rowArray = rowString.split('');
+
+
+    const spanOne = document.createElement("span");
+    const spanTwo = document.createElement("span");
+
+    spanOne.classList.add("bg-[#F7F8F8]", "rounded-lg", "flex", "items-center", "justify-center", "transition-all", "cursor-pointer", "hover:bg-[#bcbcbc85]");
+    spanOne.innerText = `${rowArray[rowID]}${sitNumber+1}`;
+
+    spanTwo.classList.add("justify-center", "items-center", "flex", "cursor-pointer");
+    spanTwo.innerText = `${rowArray[rowID]}`;
+
+    if(sitNumber === 0 || sitNumber === 1){
+
+        document.getElementById("sit-1").appendChild(spanOne);
+
+    }else{
+
+        document.getElementById("sit-2").appendChild(spanOne);
+
+    }
+
+    
+    if(sitNumber === 3){
+        document.getElementById("row-id").appendChild(spanTwo);
+        console.log(rowID);
+        rowID++;
+        
+    }
+
+}
